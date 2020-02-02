d3f420d318e28a1dc92598d7c609f47bbb40b905
maxwellacioli <maxwell.acioli@gmail.com>
2020-02-02 13:53:38 -0300

    added request validation for login session and create user

--- fastfeet/src/app/controllers/UserController.js
+++ fastfeet/src/app/controllers/UserController.js
@@ -1,8 +1,43 @@
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
+  async show(req, res) {
+    const users = await User.findAll();
+
+    const usersInfo = [];
+
+    users.map(user => {
+      const { id, name, email, provider } = user;
+
+      usersInfo.push({
+        id,
+        name,
+        email,
+        provider,
+      });
+
+      return undefined;
+    });
+
+    return res.json(usersInfo);
+  }
+
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation error.' });
+    }
+
     const { email } = req.body;
 
     const userExists = await User.findOne({ where: { email } });
--- fastfeet/src/routes.js
+++ fastfeet/src/routes.js
@@ -2,8 +2,11 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
+routes.get('/users', authMiddleware, UserController.show);
 routes.post('/users', UserController.store);
 routes.post('/login', SessionController.store);
 
