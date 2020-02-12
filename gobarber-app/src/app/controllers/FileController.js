de61e51626be1216bf2dfbcc49a23d504215a58d
Maxwell Acioli <maxwell.acioli@gmail.com>
2020-02-12 10:00:47 -0300

    Feature: update avatar_id to an user

--- gobarber-app/src/app/controllers/FileController.js
+++ gobarber-app/src/app/controllers/FileController.js
@@ -1,10 +1,18 @@
 import File from '../models/File';
+import User from '../models/User';
 
 class FileController {
   async store(req, res) {
     const { originalname: name, filename: path } = req.file;
 
     const file = await File.create({ name, path });
+
+    const user = await User.findByPk(req.userId);
+
+    user.avatar_id = file.id;
+
+    await user.save(user);
+
     return res.json(file);
   }
 }
