0981991a97173e09be8b6a832950a2936b6b9a09
maxwellacioli <maxwell.acioli@gmail.com>
2020-02-11 21:22:46 -0300

    added errors treament

--- gobarber-app/src/app.js
+++ gobarber-app/src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+import 'express-async-errors';
 import routes from './routes';
+
 import './database';
 
 class App {
@@ -10,11 +15,20 @@ class App {
      */
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandle();
   }
 
   middlewares() {
+    /*
+      Configuração do sentry seguindo a documentação:
+      -The request handler must be the first middleware on the app
+     */
+    this.server.use(Sentry.Handlers.requestHandler());
+
     /*
       Define que as informações repassadas pelas requisições e respostas
       seguem o formato json
@@ -34,6 +48,21 @@ class App {
       Adiciona todas as rotas que o servidor comporta
      */
     this.server.use(routes);
+
+    /*
+      Configuração do sentry seguindo a documentação:
+      -The error handler must be before any other error middleware and after
+      all control
+     */
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandle() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
