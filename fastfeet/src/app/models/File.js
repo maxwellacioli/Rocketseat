bc321e1df33fa2d5cd9487f3a5890f0a90be73bf
maxwellacioli <maxwell.acioli@gmail.com>
2020-02-12 21:37:28 -0300

    removed param unecessary

--- fastfeet/src/app/models/File.js
+++ fastfeet/src/app/models/File.js
@@ -4,15 +4,8 @@ class File extends Model {
   static init(sequelize) {
     super.init(
       {
-        /*
-          name e path são os elementos necessário para
-          serem armazenados no tabela files
-        */
         name: Sequelize.STRING,
         path: Sequelize.STRING,
-        /*
-          Campo url, não existe na tabela
-         */
         url: {
           type: Sequelize.VIRTUAL,
           get() {
@@ -21,9 +14,6 @@ class File extends Model {
         },
       },
       {
-        /*
-          por default o segundo elemento deve ser o sequelize
-         */
         sequelize,
       }
     );
