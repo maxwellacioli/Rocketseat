d256942dc00cfd6d934f62e2b27201eb7eb8e58d
maxwellacioli <maxwell.acioli@gmail.com>
2020-02-15 00:20:27 -0300

    create delivery index

--- fastfeet/src/app/controllers/DeliveryController.js
+++ fastfeet/src/app/controllers/DeliveryController.js
@@ -1,7 +1,35 @@
+import * as Yup from 'yup';
 import Courier from '../models/Courier';
 import Order from '../models/Order';
 
 class DeliveryController {
+  async index(req, res) {
+    const schema = Yup.object().shape({
+      done: Yup.boolean().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation error.' });
+    }
+
+    const { done } = req.body;
+    const { courierId } = req.params;
+
+    if (!done) {
+      const orders = await Order.find({
+        where: {
+          courier_id: courierId,
+          end_date: null,
+          canceled_at: null,
+        },
+      });
+
+      return res.json(orders);
+    }
+
+    return res.json();
+  }
+
   async update(req, res) {
     const { courierId, orderId } = req.params;
 
