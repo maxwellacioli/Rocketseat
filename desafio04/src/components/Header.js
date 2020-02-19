89a19c9ee0f0e7f384db7780652497e18dc428c5
Maxwell Acioli <maxwell.acioli@gmail.com>
2020-02-19 10:39:47 -0300

    created desafio04 project

---
+++ desafio04/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import '../css/Header.css'
+
+class Header extends Component {
+  render() {
+    return <div>
+      <p>
+        <h2>facebook</h2>
+        <h2>Meu perfil</h2>
+      </p>
+    </div>
+  };
+}
+
+export default Header;
