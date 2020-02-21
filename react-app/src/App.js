970a3a355310092b27c15bf1d00e050f2c024e21
maxwellacioli <maxwell.acioli@gmail.com>
2020-02-20 22:25:21 -0300

    added routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
+import Routes from './routes';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <Routes />
     </div>
   );
 }
