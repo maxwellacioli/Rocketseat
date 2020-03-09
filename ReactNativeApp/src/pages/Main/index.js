7716c2be471cebb0482fde9e1b2c739600a8d571
Maxwell Acioli <maxwell.acioli@gmail.com>
2020-03-09 15:44:51 -0300

    Save data into device storage

--- ReactNativeApp/src/pages/Main/index.js
+++ ReactNativeApp/src/pages/Main/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import AsyncStorage from '@react-native-community/async-storage';
 import api from '../../services/api';
 import {
   Container,
@@ -22,6 +24,22 @@ export default class Main extends Component {
     newUser: '',
     users: [],
     loading: false,
+  }
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  async componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
   }
 
   handleSubmit = async () => {
