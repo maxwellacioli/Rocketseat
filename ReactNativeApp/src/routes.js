7087729c30b09dc1765c3036c2d2b132b04680ff
maxwellacioli <maxwell.acioli@gmail.com>
2020-03-08 23:35:56 -0300

    Fix navigation stack error

--- ReactNativeApp/src/routes.js
+++ ReactNativeApp/src/routes.js
@@ -1,14 +1,23 @@
-import {createAppContainer} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
 
 import Main from './pages/Main';
 import User from './pages/User';
 
-const Routes = createAppContainer(
-  createStackNavigator({
-    Main,
-    User,
-  })
-);
+const Stack = createStackNavigator();
 
-export default Routes;
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{title: 'Usuários'}}
+        />
+        <Stack.Screen name="User" component={User} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
