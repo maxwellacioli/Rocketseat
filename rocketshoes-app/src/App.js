1d798cb883740934c08e1697885ccfcc5eecbce3
maxwellacioli <maxwell.acioli@gmail.com>
2020-04-08 11:53:47 -0300

    Add header style

--- rocketshoes-app/src/App.js
+++ rocketshoes-app/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from './styles/global';
+import Header from './compoonents/Header';
+
 import Routes from './routes';
 
 function App() {
   return (
     <>
-      <Routes />
+      <GlobalStyles />
+      <BrowserRouter>
+        <Header />
+        <Routes />
+      </BrowserRouter>
     </>
   );
 }
