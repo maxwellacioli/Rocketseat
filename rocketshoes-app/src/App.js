3f8ebc4560b21e2e7abdad9694f5204241c40a71
maxwellacioli <maxwell.acioli@gmail.com>
2020-04-08 15:08:05 -0300

    Add cart reducer

--- rocketshoes-app/src/App.js
+++ rocketshoes-app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import GlobalStyles from './styles/global';
-import Header from './compoonents/Header';
+import Header from './components/Header';
 
 import Routes from './routes';
 
