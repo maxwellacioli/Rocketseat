231232caeeb3c902356d19bc691c89e0571c68e2
maxwellacioli <maxwell.acioli@gmail.com>
2020-04-08 14:41:53 -0300

    Configure redux

--- rocketshoes-app/src/App.js
+++ rocketshoes-app/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyles from './styles/global';
 import Header from './compoonents/Header';
 
 import Routes from './routes';
 
+import store from './store/index';
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyles />
       <BrowserRouter>
         <Header />
         <Routes />
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
