c0d09948b9d0dcf139291bd78e10be7b61d7bc1c
Maxwell <maxwell.acioli@gmail.com>
2020-04-10 21:42:07 -0300

    Add toastify

--- rocketshoes-app/src/store/modules/cart/sagas.js
+++ rocketshoes-app/src/store/modules/cart/sagas.js
@@ -1,11 +1,40 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, select, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import * as CartActions from './actions';
+import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
 
-  yield put(CartActions.addToCartSucess(response.data));
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora de estoque.');
+
+    return;
+  }
+
+  if (productExists) {
+    yield put(CartActions.updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      formattedPrice: formatPrice(response.data.price),
+    };
+
+    yield put(CartActions.addToCartSucess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
