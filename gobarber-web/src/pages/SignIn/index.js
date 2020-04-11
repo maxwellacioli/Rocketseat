909a2ef5bef484f33b8735d099a90383f8fec4f4
Maxwell <maxwell.acioli@gmail.com>
2020-04-11 16:41:28 -0300

    Add auth styles

--- gobarber-web/src/pages/SignIn/index.js
+++ gobarber-web/src/pages/SignIn/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
+import logo from '~/assets/images/logo.svg';
+
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarberLogo" />
+      <form>
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Registrar-se</Link>
+      </form>
+    </>
+  );
 }
--- gobarber-web/src/pages/SignUp/index.js
+++ gobarber-web/src/pages/SignUp/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
+import logo from '~/assets/images/logo.svg';
+
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarberLogo" />
+      <form>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Registrar</button>
+        <Link to="/">Já possuo login</Link>
+      </form>
+    </>
+  );
 }
