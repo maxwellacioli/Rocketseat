36b034ff9bc6637bce62607cbbad7bdd4e1010f0
maxwellacioli <maxwell.acioli@gmail.com>
2020-04-30 12:57:34 -0300

    Add validation to auth pages

--- gobarber-web/src/pages/SignIn/index.js
+++ gobarber-web/src/pages/SignIn/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 // import { Container } from './styles';
 
 import logo from '~/assets/images/logo.svg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido!')
+    .required('O e-mail é obrigatório!'),
+  password: Yup.string()
+    .min(6, 'A senha deve ter 6 caracteres no mínimo')
+    .required('A senha é obrigatória!'),
+});
+
 export default function SignIn() {
+  function handleSubmit(data) { }
+
   return (
     <>
       <img src={logo} alt="GoBarberLogo" />
-      <form>
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="text" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Registrar-se</Link>
-      </form>
+      </Form>
     </>
   );
 }
