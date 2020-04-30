72c2ed42055c62c74a634706c7b3eb2c6a90ef7b
maxwellacioli <maxwell.acioli@gmail.com>
2020-04-30 16:16:15 -0300

    Add auth actions

--- gobarber-web/src/store/modules/auth/sagas.js
+++ gobarber-web/src/store/modules/auth/sagas.js
@@ -1,3 +1,25 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import api from '../../../services/api';
+import history from '../../../services/history';
+import * as Actions from './actions';
 
-export default all([]);
+function* singIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, '/sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuário não é prestador de serviço');
+  }
+
+  yield put(Actions.signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', singIn)]);
